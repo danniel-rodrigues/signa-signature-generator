882276141ecc3579c5842273a7a3a5dc769fa5fb
Daniel Santos Rodrigues <danielrodrigues.dev@gmail.com>
2025-05-22 23:04:22 -0300

    feat: add function for  signature live preview

---
+++ script.js
@@ -0,0 +1,20 @@
+let inputs = document.querySelectorAll("input");
+let previews = document.querySelectorAll("p");
+
+// Atualiza o conteúdo do card de assinatura com as informações passadas pelo usuário.
+function livePreview(inputValue, inputID) {
+  previews.forEach((preview) => {
+    // Verifica se o ID do input do usuário é igual ao do preview ou uma substring dele.
+    if (preview.id.match(inputID)) {
+      // Atualiza cada campo do preview com as informções do usuário em tempo real.
+      preview.textContent = inputValue;
+    }
+  });
+}
+
+// Para cada input, é adicionado um EventListener para capturar as informações do usuário.
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    livePreview(input.value, input.id);
+  });
+});
